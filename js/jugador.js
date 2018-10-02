fd0723ac10178646db1a4181ff04ea9cae802d17
John-Debon <matudbonis@gmail.com>
2018-10-02 17:33:26 -0300

    added movement and life losing methods

--- js/jugador.js
+++ js/jugador.js
@@ -12,6 +12,28 @@ var Jugador = {
     vidas: 5,
     // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
     // y todo lo que haga falta para que cumpla con sus responsabilidades
-    moverJugador: function name(params) {},
-    perderVidas: function name(params) {}
+    moverJugador: function moverJugador(movX, movY, tecla) {
+        this.x += movX;
+        this.y += movY;
+        if (tecla === 'arriba') {
+            this.sprite = 'imagenes/auto_rojo_arriba.png';
+            this.ancho = 15;
+            this.alto = 30;
+        } else if (tecla === 'abajo') {
+            this.sprite = 'imagenes/auto_rojo_abajo.png';
+            this.ancho = 15;
+            this.alto = 30;
+        } else if (tecla === 'der') {
+            this.sprite = 'imagenes/auto_rojo_derecha.png';
+            this.ancho = 30;
+            this.alto = 15;
+        } else if (tecla === 'izq') {
+            this.sprite = 'imagenes/auto_rojo_izquierda.png';
+            this.ancho = 30;
+            this.alto = 15;
+        }
+    },
+    perderVidas: function perderVidas(potencia) {
+        this.vidas -= potencia;
+    }
 };
