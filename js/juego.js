5c6f68702e5b4fe3297577e5e3dfe44b8869cada
John-Debon <matudbonis@gmail.com>
2018-10-12 18:55:31 -0300

    Added enemy damage

--- js/juego.js
+++ js/juego.js
@@ -249,11 +249,9 @@ se ven las colisiones con los obstaculos. En este caso sera con los zombies. */
 Juego.calcularAtaques = function() {
     this.enemigos.forEach(function(enemigo) {
         if (this.intersecan(enemigo, this.jugador, this.jugador.x, this.jugador.y)) {
-            /* Si el enemigo colisiona debe empezar su ataque
-      COMPLETAR */
+            enemigo.comenzarAtaque();
         } else {
-            /* Sino, debe dejar de atacar
-      COMPLETAR */
+            enemigo.dejarDeAtacar();
         }
     }, this);
 };
